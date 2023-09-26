189985ad847a0d181b757ba6304ff93b5bef06c0
Caffery Yang <cafferychen7850@gmail.com>
2023-09-26 10:44:01 +0800

    Update generate_taxa_indiv_change_scatterplot_pair.R

--- R/generate_taxa_indiv_change_scatterplot_pair.R
+++ R/generate_taxa_indiv_change_scatterplot_pair.R
@@ -423,8 +423,10 @@ generate_taxa_indiv_change_scatterplot_pair <-
         # Close the PDF device
         dev.off()
       }
+      names(plot_list) <- taxa.levels
       return(plot_list)
     })
 
+    names(plot_list_all) <- feature.level
     return(plot_list_all)
   }
