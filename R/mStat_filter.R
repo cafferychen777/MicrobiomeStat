50fd7e745b0480efd5ed12f67602dd4d888052ef
Caffery Yang <cafferychen7850@gmail.com>
2023-10-10 19:46:07 +0800

    Update mStat_filter.R

--- R/mStat_filter.R
+++ R/mStat_filter.R
@@ -1,41 +1,50 @@
-#' Filter a Microbiome Data Frame by Prevalence and Abundance
+#' Filter a Microbiome Data Matrix by Prevalence and Average Abundance
 #'
-#' This function filters taxa in a microbiome data frame based on a minimum
-#' prevalence and average abundance threshold.
+#' This function filters taxa in a microbiome matrix based on specified
+#' prevalence and average abundance thresholds.
 #'
-#' @param x A matrix or data frame containing taxa (in rows) by sample (in columns)
-#' microbial abundance data.
+#' @param x A matrix containing taxa (in rows) and sample (in columns) microbial abundance data.
 #' @param prev.filter Numeric, the minimum prevalence threshold for a taxon to be
 #' retained. Prevalence is calculated as the proportion of samples where the
 #' taxon is present.
 #' @param abund.filter Numeric, the minimum average abundance threshold for a taxon
 #' to be retained.
 #'
-#' @return A matrix or data frame with filtered taxa based on the specified thresholds.
+#' @return A matrix with taxa filtered based on the specified thresholds.
 #'
 #' @details
-#' The function first reshapes the input data to long format and then groups by taxa
-#' to compute the average abundance and prevalence. After this, it filters out taxa
-#' based on the provided prevalence and abundance thresholds.
+#' The function first converts the input data into a long format and then groups by taxa.
+#' It computes both the average abundance and prevalence for each taxon. Subsequently, it
+#' filters out taxa that do not meet the provided prevalence and average abundance thresholds.
 #'
 #' @examples
+#'
+#' # Example with simulated data
 #' \dontrun{
 #' data_matrix <- matrix(c(0, 3, 4, 0, 2, 7, 8, 9, 10), ncol=3)
 #' colnames(data_matrix) <- c("sample1", "sample2", "sample3")
 #' rownames(data_matrix) <- c("taxa1", "taxa2", "taxa3")
 #'
-#' filtered_data <- mStat_filter(data_matrix, 0.5, 5)
-#' print(filtered_data)
+#' filtered_data_simulated <- mStat_filter(data_matrix, 0.5, 5)
+#' print(filtered_data_simulated)
 #' }
 #'
+#' # Example with real dataset: peerj32.obj
+#' \dontrun{
+#' data(peerj32.obj)
+#' data_matrix_real <- peerj32.obj$feature.tab
+#'
+#' # Assuming the matrix contains counts with taxa in rows and samples in columns
+#' filtered_data_real <- mStat_filter(data_matrix_real, 0.5, 5)
+#' print(filtered_data_real)
+#' }
 #' @export
 mStat_filter <- function(x, prev.filter, abund.filter){
-  # 将数据框从宽格式转换为长格式
+
   x_long <- as.data.frame(as.table(as.matrix(x)))
 
-  # 计算每个taxa的平均abundance和prevalence
   filtered_taxa <- x_long %>%
-    dplyr::group_by(Var1) %>%  # Var1是taxa
+    dplyr::group_by(Var1) %>%
     dplyr::summarise(
       avg_abundance = mean(Freq),
       prevalence = sum(Freq > 0) / dplyr::n()
