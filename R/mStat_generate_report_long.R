755c4e3fe11ae6fe872648c137aff90036d02ccf
Caffery Yang <cafferychen7850@gmail.com>
2023-10-28 13:00:20 +0800

    Update mStat_generate_report_long.R

--- R/mStat_generate_report_long.R
+++ R/mStat_generate_report_long.R
@@ -275,7 +275,7 @@ mStat_generate_report_long <- function(data.obj,
   template <- "
 ---
 title: '`r sub(\".pdf$\", \"\", basename(output.file))`'
-author: '[Powered by MicrobiomeStat (Ver 1.1.1)](http://www.microbiomestat.wiki)'
+author: '[Powered by MicrobiomeStat (Ver 1.1.2)](http://www.microbiomestat.wiki)'
 date: '`r Sys.Date()`'
 output:
   pdf_document:
