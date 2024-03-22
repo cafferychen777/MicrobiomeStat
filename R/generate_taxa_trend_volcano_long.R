40506475d078b4d0de67c0a77125d8a74761ae84
Caffery Yang <cafferychen7850@gmail.com>
2024-03-22 09:46:35 +0800

    Update generate_taxa_trend_volcano_long.R

--- R/generate_taxa_trend_volcano_long.R
+++ R/generate_taxa_trend_volcano_long.R
@@ -95,7 +95,7 @@ generate_taxa_trend_volcano_long <-
               geom_point() +
               geom_vline(aes(xintercept = 0), linetype = "dashed", linewidth = 1.5, color = "grey") +
               geom_hline(aes(yintercept = -log10(feature.sig.level)), linetype = "dashed", linewidth = 1.5, color = "grey") +
-              geom_text_repel(
+              ggrepel::geom_text_repel(
                 aes(label = ifelse(get(p_val_var) < feature.sig.level, as.character(Variable), '')),
                 size = 3.5,
                 box.padding = 0.35,
@@ -144,7 +144,7 @@ generate_taxa_trend_volcano_long <-
             geom_point() +
             geom_vline(aes(xintercept = 0), linetype = "dashed", linewidth = 1.5, color = "grey") +
             geom_hline(aes(yintercept = -log10(feature.sig.level)), linetype = "dashed", linewidth = 1.5, color = "grey") +
-            geom_text_repel(
+            ggrepel::geom_text_repel(
               aes(label = ifelse(get(p_val_var) < feature.sig.level, as.character(Variable), '')),
               size = 3.5,
               box.padding = 0.35,
