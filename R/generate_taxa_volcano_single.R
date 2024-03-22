d84700efd220229e1f693e0e5a56bf7d35a01bc6
Caffery Yang <cafferychen7850@gmail.com>
2024-03-22 09:46:41 +0800

    Update generate_taxa_volcano_single.R

--- R/generate_taxa_volcano_single.R
+++ R/generate_taxa_volcano_single.R
@@ -114,7 +114,7 @@ generate_taxa_volcano_single <-
               linewidth = 1.5,
               color = "grey"
             ) +
-            geom_text_repel(
+            ggrepel::geom_text_repel(
               aes(label = ifelse(
                 get(p_val_var) < feature.sig.level,
                 as.character(Variable),
